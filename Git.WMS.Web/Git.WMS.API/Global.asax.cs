7f6e153484176b27fb3f1985e212443c80b3dad3
Jim <376720513@qq.com>
2020-04-02 18:14:29 +0800

    add

--- Git.WMS.Web/Git.WMS.API/Global.asax.cs
+++ Git.WMS.Web/Git.WMS.API/Global.asax.cs
@@ -10,6 +10,7 @@ using System.Web.Routing;
 
 namespace Git.WMS.API
 {
+
     // 注意: 有关启用 IIS6 或 IIS7 经典模式的说明，
     // 请访问 http://go.microsoft.com/?LinkId=9394801
     public class MvcApplication : System.Web.HttpApplication
